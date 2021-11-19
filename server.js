3a00e9bc82d99f3d57cd054110a6af9266460994
Tali <taliitacamargo@icloud.com>
2021-11-19 13:56:30 -0500

    api

--- server.js
+++ server.js
@@ -3,16 +3,17 @@ const path = require('path');
 const fs = require('fs');
 const { readFromFile } = require("./public/assets/helpers/fsUtils");
 const uuid = require('./public/assets/helpers/uuid');
-
-
 var notes = require('./db/db.json');
+const api = require('./public/assets/js/index')
 
 const PORT = process.env.port || 3001;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 app.use(express.static('public'));
+
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
